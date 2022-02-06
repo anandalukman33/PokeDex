1fbee4356fb001996a8ddfa228fb8085a162814a
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-02-07 02:59:16 +0700

    Initial Theme UI PokeDex

--- app/src/main/java/id/my/anandalukman/pokedex/MainActivity.kt
+++ app/src/main/java/id/my/anandalukman/pokedex/MainActivity.kt
@@ -1,11 +1,22 @@
 package id.my.anandalukman.pokedex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import id.my.anandalukman.pokedex.ui.theme.JetpackComposePokedexTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            JetpackComposePokedexTheme {
+
+            }
+        }
     }
 }
